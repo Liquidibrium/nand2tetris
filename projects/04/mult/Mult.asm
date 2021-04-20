98ce24b5c8b2f661f42e7d00953fd8698f983390
1O <vanoganjelashvili@gmail.com>
2021-04-20 14:35:03 +0400

    mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,49 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// n = R0
+// m = R1
+// mult = 0
+// LOOP:
+//     if m == 0 goto STOP
+//     m -= 1
+//     mult +=n
+//     goto LOOP
+// STOP:
+//     R2 = mult
+
+@R0
+D=M
+@n
+M=D // n = R0
+@R1
+D=M
+@m 
+M=D // m =R1 
+@mult 
+M=0 // mult = 0
+(LOOP)
+@m
+D=M
+@STOP 
+D;JEQ // if m == 0 goto STOP
+D=D-1
+@m 
+M=D
+@mult
+D=M
+@n
+D=D+M
+@mult
+M=D // mult = mult + n
+@LOOP
+0;JMP
+(STOP)
+@mult
+D=M
+@R2
+M=D // RAM[2] = mult
+(END)
+@END 
+0;JMP 
