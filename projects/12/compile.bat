e69d320d8416007a2eb088fd9d7ed3c37401d35a
Liquidibrium <vanoganjelashvili@gmail.com>
2021-06-22 22:29:14 +0400

    started project 12

---
+++ projects/12/compile.bat
@@ -0,0 +1,16 @@
+copy Array.jack .\ArrayTest
+start /b C:\Users\vganjelashvili\Desktop\cs\nand2tetris\tools\JackCompiler.bat .\ArrayTest
+@REM start /b C:\Users\vganjelashvili\Desktop\cs\nand2tetris\tools\VMEmulator.bat .\ArrayTest\ArrayTest.tst
+
+copy Keyboard.jack .\KeyboardTest
+start /b C:\Users\vganjelashvili\Desktop\cs\nand2tetris\tools\JackCompiler.bat .\KeyboardTest
+copy Math.jack .\MathTest
+start /b C:\Users\vganjelashvili\Desktop\cs\nand2tetris\tools\JackCompiler.bat .\MathTest
+copy Memory.jack .\MemoryTest
+start /b C:\Users\vganjelashvili\Desktop\cs\nand2tetris\tools\JackCompiler.bat .\MemoryTest
+copy String.jack .\StringTest
+start /b C:\Users\vganjelashvili\Desktop\cs\nand2tetris\tools\JackCompiler.bat .\StringTest
+copy Sys.jack .\SysTest
+start /b C:\Users\vganjelashvili\Desktop\cs\nand2tetris\tools\JackCompiler.bat .\SysTest
+copy Screen.jack .\ScreenTest
+start /b C:\Users\vganjelashvili\Desktop\cs\nand2tetris\tools\JackCompiler.bat .\ScreenTest
